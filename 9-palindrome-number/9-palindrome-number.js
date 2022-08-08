7fcd13e32c6d708a3c10fb294d628ec51ca70bc7
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2022-08-08 10:09:17 +0100

    Time: 281 ms (49.69%), Space: 50.7 MB (74.91%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -5,6 +5,7 @@
 var isPalindrome = function(x) {
     if(x < 0) return false;
     if(x < 10) return true;
+    if( x % 10 === 0) return false;
     const str = x.toString();
     const strRev = str.split('').reverse().join('');
     return str === strRev;
