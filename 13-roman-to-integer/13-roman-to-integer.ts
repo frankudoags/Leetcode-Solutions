e62a01b14fe57b421960343f356fdca9888771a0
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2022-08-12 13:29:16 +0100

    Time: 199 ms (67.75%), Space: 48.6 MB (47.81%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.ts
+++ 13-roman-to-integer/13-roman-to-integer.ts
@@ -1,15 +1,15 @@
 function romanToInt(s: string): number {
     const map: object = { M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 };
-   let result : number[] = [];
+   let result : number = 0;
    for(let i = 0; i < s.length; i++) {
        if(map[s[i]] < map[s[i + 1]]) {
-              result.push(map[s[i + 1]] - map[s[i]]);
+              result += (map[s[i + 1]] - map[s[i]]);
               i++;
        }
        else {
-              result.push(map[s[i]]);
+              result+= (map[s[i]]);
        }
    }
 
-   return result.reduce((acc, curr) => acc + curr, 0);
+   return result;
 };
