595f731124ac18661e745ab132b3d68294e609c0
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2022-08-12 13:27:46 +0100

    Time: 166 ms (78.82%), Space: 47.7 MB (41.38%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -4,16 +4,16 @@
  */
 var romanToInt = function(s) {
     const map = { M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 };
-    let result = [];
+    let result = 0;
     for(let i = 0; i < s.length; i++) {
        if(map[s[i]] < map[s[i + 1]]) {
-              result.push(map[s[i + 1]] - map[s[i]]);
+              result += (map[s[i + 1]] - map[s[i]]);
               i++; 
        }
        else {
-              result.push(map[s[i]]);
+              result +=(map[s[i]]);
        }
    }
 
-   return result.reduce((acc, curr) => acc + curr, 0);
+   return result;
 };
