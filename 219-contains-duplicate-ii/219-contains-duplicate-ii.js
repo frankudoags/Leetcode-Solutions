c91490761a4ce3628e7946474dc00688ea268619
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2022-08-15 08:31:26 +0100

    Time: 173 ms (64.13%), Space: 67.4 MB (17.25%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.js
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.js
@@ -3,13 +3,26 @@
  * @param {number} k
  * @return {boolean}
  */
+//O(n^2) time, O(1) space
+// var containsNearbyDuplicate = function(nums, k) {
+//     for(let i=0; i<nums.length; i++){
+//         for(let j=i+1;j<nums.length; j++){
+//             if((nums[i]==nums[j]) && (Math.abs(i-j)<=k)){
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// };
+
+
+//optimized solution using maps to slow down time
+//O(n) time, O(n) space
 var containsNearbyDuplicate = function(nums, k) {
-    for(let i=0; i<nums.length; i++){
-        for(let j=i+1;j<nums.length; j++){
-            if((nums[i]==nums[j]) && (Math.abs(i-j)<=k)){
-                return true;
-            }
-        }
+    let map = new Map();
+    for (const [i , n] of nums.entries()) {
+        if(map.has(n) && (i - map.get(n)) <=k ) return true;
+        map.set(n , i);
     }
     return false;
-};
+}
