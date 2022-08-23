79b99020b702abd5c3895441e332a66be440c449
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2022-08-23 21:27:16 +0100

    Time: 126 ms (17.55%), Space: 42 MB (97.64%) - LeetHub

--- 67-add-binary/67-add-binary.js
+++ 67-add-binary/67-add-binary.js
@@ -3,6 +3,4 @@
  * @param {string} b
  * @return {string}
  */
-const addBinary = (a, b) => {
-return (BigInt("0b" + a) + BigInt("0b" + b)).toString(2);
-};
+const addBinary = (a, b) =>  (BigInt("0b" + a) + BigInt("0b" + b)).toString(2);
