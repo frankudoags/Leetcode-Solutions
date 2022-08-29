ab498412f61458681d8ceb12fd8bc7e290c691d2
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2022-08-29 08:53:11 +0100

    Time: 132 ms (21.06%), Space: 44.4 MB (15.59%) - LeetHub

--- 258-add-digits/258-add-digits.js
+++ 258-add-digits/258-add-digits.js
@@ -5,9 +5,7 @@
 var addDigits = function(num) {
     while(num > 9) {
         num = num.toString().split('');
-        console.log(num);
         num = num.reduce((acc, cur) => +acc + +cur, 0);
-        console.log(num);
     };
     
     return num;
