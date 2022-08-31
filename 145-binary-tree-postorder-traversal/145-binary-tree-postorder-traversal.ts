a53e40a036481296fca13906964b1ef1deb13904
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2022-08-31 15:42:50 +0100

    Time: 104 ms (44.71%), Space: 44.1 MB (79.33%) - LeetHub

---
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function postorderTraversal(root: TreeNode | null): number[] {
+     let array: number[] = [];
+    const traverse = (node: TreeNode) => {
+        if(node){
+           traverse(node.left);
+            traverse(node.right);
+            array.push(node.val);
+         }
+    }
+    traverse(root);
+    return array;
+};
