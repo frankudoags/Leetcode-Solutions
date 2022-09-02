0e220d38526b16b1272daf4703260fbbba0d644e
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2022-09-02 19:16:26 +0100

    Time: 151 ms (37.15%), Space: 49.1 MB (74.77%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -3,16 +3,13 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let leftPtr = 0;
+     let leftPtr = 0;
     let rightPtr = s.length -1;
     
    while(leftPtr < rightPtr) {
-       let temp = s[leftPtr];
-       s[leftPtr] = s[rightPtr];
-       s[rightPtr] = temp;
+       [s[leftPtr], s[rightPtr]] = [s[rightPtr], s[leftPtr]]
 
        leftPtr++;
        rightPtr--;
    }
-    return s;
 };
