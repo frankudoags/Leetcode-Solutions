213e5fb664f56f9d6e136c9cb22e4a1a06d4bc18
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2022-09-02 18:34:19 +0100

    Time: 79 ms (82.76%), Space: 43.1 MB (86.74%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.js
+++ 409-longest-palindrome/409-longest-palindrome.js
@@ -3,7 +3,9 @@
  * @return {number}
  */
 var longestPalindrome = function(s) {
+    //Helper map to keep track of the objects
     let map = new Map();
+    //Count to keep track of pairs 
     let count = 0;
     
     for(let i = 0; i < s.length; i++) {
@@ -22,3 +24,20 @@ var longestPalindrome = function(s) {
     
     return count;
 };
+/**
+Create a map and iterate through the length of the string
+For each item, we check if it is in the map,
+if it is, we increment count to 2 and we delete the character.
+
+example: 
+aaabbb
+we set first a to map {a:1,}
+then we see second a, we increment count by 2, as we have found a pair and they'll
+be at the palindrome start and end.
+
+
+We continue until we are done with the string.
+We must also check that the size of the map is not more than zero.
+If it is, we increment count by 1, to account for a single character that
+can be added to the existing longest palindrome.
+*/
