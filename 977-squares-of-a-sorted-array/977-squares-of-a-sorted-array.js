04fbae8e7aaa1c96bca61713b5ce4d3d6508ef9f
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2022-09-27 17:14:00 +0100

    Time: 178 ms (44.41%), Space: 49 MB (21.90%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    const result = [];
+    for(let i = 0; i < nums.length; i++) {
+        result.push(nums[i]*nums[i]);
+    }
+    result.sort((a,b) => a - b);
+    return result;
+};
