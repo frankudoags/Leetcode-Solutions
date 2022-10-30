3923dc397b14f58796662b9c6725602c22747e9b
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2022-10-30 17:47:19 +0100

    Time: 106 ms (64.37%), Space: 44.2 MB (48.41%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.ts
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const map = new Map();
+    
+    for(let i = 0; i < nums.length; i++) {
+        if(!map.has(nums[i])) map.set(nums[i], 1);
+        else map.set(nums[i], (map.get(nums[i]) + 1));
+    }
+    let answer;
+    let max = 0;   
+    for(const [key, value] of map) {
+        if(value > max) {
+            max = value;
+            answer = key;
+        }
+    }
+    return answer;
+};
