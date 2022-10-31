81bf620116585188192d3531f0d0feda858eef90
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2022-10-31 13:31:43 +0100

    Time: 122 ms (71.24%), Space: 45.1 MB (93.28%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.ts
@@ -0,0 +1,11 @@
+function findErrorNums(nums: number[]): number[] {
+      let dupe: number, missing: number, N: number = nums.length;
+    for(let i = 0; i < N; i++){
+        nums[(nums[i] - 1) % 10_000] += 10_000;
+    }
+    for(let i = 0; i < N; i++){
+        if(nums[i] > 20_000) dupe = i + 1;
+        if(nums[i] < 10001) missing = i + 1;
+    }
+    return [dupe, missing]
+};
