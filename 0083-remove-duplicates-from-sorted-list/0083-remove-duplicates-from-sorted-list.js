eda48cfbd617f3dd745bfb27625bfa3611ed9900
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-02-06 21:47:00 +0100

    Time: 76 ms (62.89%), Space: 44.2 MB (51.92%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let curr =  head;
+    
+    while(curr && curr.next){
+        if(curr.next.val === curr.val){
+            curr.next = curr.next.next;
+            continue;
+        } else {
+            curr = curr.next;
+        }
+    }
+        
+    
+    return head
+};
