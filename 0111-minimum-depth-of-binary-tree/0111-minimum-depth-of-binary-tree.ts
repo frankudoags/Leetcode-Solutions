cfb7f9279906c0a9524abd43158f505decad9891
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-02-12 23:31:52 +0100

    Time: 261 ms (68.70%), Space: 83.5 MB (50.43%) - LeetHub

---
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function minDepth(root: TreeNode | null): number {
+    if(!root) return 0;
+    if(root.right !== null && root.left !== null){
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    } else {
+        return 1 + Math.max(minDepth(root.left), minDepth(root.right));
+    }
+};
