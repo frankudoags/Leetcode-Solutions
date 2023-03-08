0dc6d1a5012c004354e345604d9e4cfec025f2a7
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-03-08 12:00:09 +0100

    Time: 96 ms (56.51%), Space: 45.3 MB (59.04%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -0,0 +1,22 @@
+function isAnagram(s: string, t: string): boolean {
+    if (s.length !== t.length) return false;
+    
+    let map: Record<string, number> = {};
+    
+    for(let i = 0; i < s.length; i++){
+        if(!map[s[i]]) map[s[i]] = 0;
+        map[s[i]]++;
+        
+        if(!map[t[i]]) map[t[i]] = 0;
+        map[t[i]]--;
+        
+    }
+    let ans = true;
+    for(const [key, val] of Object.entries(map)){
+    if(val !== 0){
+        ans = false;
+        break;
+    }
+    }
+    return ans;
+};
