298441d7cb7687fa23db7128cefd642de236f08f
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-07-03 20:53:22 +0100

    Time: 0 ms (100.00%), Space: 2.4 MB (85.62%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.rs
+++ 0206-reverse-linked-list/0206-reverse-linked-list.rs
@@ -16,15 +16,30 @@
 // }
 impl Solution {
     pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+            //hold reverse
             let mut rev = None;
+        
+            //mutable reference of head of linkedlist
             let mut curr = head;
-            
+        
+            //while loop that checks that the current element
+            //in the linkedlist is not a None type
             while let Some(mut node) = curr.take() {
+                
+                //store a ref to the rest of the linked list
                 let mut next = node.next.take();
+                
+                //assign current element .next to rev, (main reversing technique)
                 node.next = rev;
+                
+                //restore rev to new reversed list, store in a Some() type
                 rev = Some(node);
+                
+                //move curr to the ref of the rest we stored initially
                 curr = next;
             }
+        
+            //return rev
             rev
         
     }
