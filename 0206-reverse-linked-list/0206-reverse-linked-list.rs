d95ad5837ff829ab2ee17b7520e4984b3cf98ed7
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-07-03 20:50:19 +0100

    Time: 1 ms (76.47%), Space: 2.3 MB (85.62%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.rs
@@ -0,0 +1,35 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        // if head.is_none() || head.as_mut().unwrap().next.is_none() {
+        //     return head;
+        // } else {
+            let mut rev = None;
+            let mut curr = head;
+            
+            while let Some(mut node) = curr.take() {
+                let mut next = node.next.take();
+                node.next = rev;
+                rev = Some(node);
+                curr = next;
+            }
+            rev
+        // }
+        
+    }
+}
