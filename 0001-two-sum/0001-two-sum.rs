ad49311d475464f232da5ca15e2f7adeed036c4a
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-07-04 21:42:04 +0100

    Time: 2 ms (82.34%), Space: 2.4 MB (38.86%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        
+        let mut hash_map = HashMap::with_capacity(nums.len());
+        
+        for (i, &num) in nums.iter().enumerate() {
+            match hash_map.get(&num) {
+                Some(&j) => return vec![i as i32, j as i32],
+                None => {
+                    hash_map.insert(target-num, i);
+                },
+            }
+        }
+        
+        unreachable!()
+    }
+}
