277d4e5a015d2130f2470622c709dbf3869ee6f2
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-07-07 13:35:50 +0100

    Time: 1 ms (80.23%), Space: 2.1 MB (82.81%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.rs
+++ 0035-search-insert-position/0035-search-insert-position.rs
@@ -2,7 +2,7 @@ impl Solution {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
         let (mut l, mut r) = (0, nums.len());
         while l < r {
-            let mid = l + ((r - l) / 2);
+            let mid = (l + r) / 2 ;
             if nums[mid] < target {
                 l = mid + 1;
             } else {
