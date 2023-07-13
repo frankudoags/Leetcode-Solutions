8ead73a6d362c435c12a55865953ccd741d9e419
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-07-13 12:12:17 +0100

    Time: 0 ms (100.00%), Space: 2 MB (87.08%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.rs
+++ 0509-fibonacci-number/0509-fibonacci-number.rs
@@ -1,9 +1,12 @@
 impl Solution {
     pub fn fib(n: i32) -> i32 {
-        if n <= 1 {
-            return n;
-        }
-        
-        return Self::fib(n - 1) + Self::fib(n - 2);
+        // create a small range from 0-n then apply fold method on it, 
+        //this is like a reduce method in javascript, it continues to apply
+        //the closure function on the first element in the fold method
+        // we use it to create a fibonnaci kind of increment and we return the
+        // second element(ie the .1 in the end of the code) as the fib answer
+        (0..n)
+        .fold((1, 0), |(first, second), _| (second, first + second) )
+        .1
     }
 }
