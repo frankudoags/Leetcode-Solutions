a110f4d922d04da681aeb2469020c0ba807e803c
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-07-16 22:33:51 +0100

    Time: 1 ms (90.91%), Space: 2.2 MB (72.73%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.rs
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.rs
@@ -21,6 +21,34 @@ use std::cell::RefCell;
 impl Solution {
     pub fn sum_of_left_leaves(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
         let mut sum = 0;
+
+        //using a recursive function, recurse and find leaves and increment sum
+        fn helper(root: &Option<Rc<RefCell<TreeNode>>>, sum: &mut i32, flag:i8) {
+            if let Some(node) = root {
+                let mut node = node.borrow();
+
+                if node.left.is_none() && node.right.is_none() && flag == 1 {
+                    *sum += node.val;
+                }
+
+                helper(&node.left, sum, 1);
+                helper(&node.right, sum, 0);
+            }
+        }
+
+        helper(&root, &mut sum, 0);
+        
+        sum
+    }
+
+}
+//APPROACH ONE: STACK METHOD
+//use stack to hold all nodes, when pushing nodes, store a flag to identify left nodes
+// and then start popping off values, if the flag is true(i.e when you find a left node), increment a sum variable, Return sum when done
+/*
+impl Solution {
+    pub fn sum_of_left_leaves(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut sum = 0;
         let mut stack = vec![(root.unwrap(), 0)];
 
         while let Some((n, is_left)) = stack.pop() {
@@ -39,5 +67,4 @@ impl Solution {
         sum
     }
 }
-//use stack to hold all nodes, when pushing nodes, store a flag to identify left nodes
-// and then start popping off values, if the flag is true(i.e when you find a left node), increment a sum variable, Return sum when done
+*/
