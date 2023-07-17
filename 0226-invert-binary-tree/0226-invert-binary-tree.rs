884a12b9b7eed97420dcb971f7d713ac277c15aa
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-07-17 08:48:36 +0100

    Time: 1 ms (79.43%), Space: 2 MB (58.86%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.rs
+++ 0226-invert-binary-tree/0226-invert-binary-tree.rs
@@ -21,18 +21,32 @@ use std::cell::RefCell;
 type TreeRef = Option<Rc<RefCell<TreeNode>>>;
 impl Solution {
     pub fn invert_tree(root: TreeRef) -> TreeRef {
+        
+        //helper function
         fn invert(root: & TreeRef) {
+            //check that node is not null
             if let Some(node) = root {
+                //borrow mutable reference
                 let mut node = node.borrow_mut();
+                //hold left node in a variable
                 let temp = node.left.take();
+                // assign swap left for right
                 node.left = node.right.take();
+                //swap right for left
                 node.right = temp;
+                //use recursion to perform the same operation for left and right nodes
                 invert(&node.left);
                 invert(&node.right);
             }
         }
+        
+        //invert the tree
         invert(&root);
+        
+        //return the tree
         root
     }
-
 }
+
+//traverse the tree, on each node, borrow mutably and swap the children
+// nodes, then recursively perform the same operation on each child node
