5bdda749a14901176e7ec747dfe39aba538748db
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-09-02 18:10:28 +0100

    Time: 106 ms (44.52%), Space: 48.2 MB (56.16%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.ts
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    let str1 = "";
+    let str2 = "";
+    
+    while(l1){
+        str1 = l1.val + str1;
+        l1 = l1.next;
+    }
+    while(l2){
+        str2 = l2.val + str2;
+        l2 = l2.next;
+    }
+    
+    let sum = BigInt(str1) + BigInt(str2);
+    
+    let sum2 = sum.toString().split('');
+
+    let dummy = new ListNode(+sum2[0]);
+    sum2.shift();
+    
+    while(sum2.length > 0) {
+        let node = new ListNode(+sum2[0]);
+        sum2.shift();
+        node.next = dummy;
+        dummy = node;
+    }
+    
+    return dummy;
+};
