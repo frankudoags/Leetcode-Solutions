69019a2264d2bea775cb1012418d25e77174b039
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-09-11 22:51:33 +0100

    Time: 118 ms (100%), Space: 87.1 MB (65%) - LeetHub

---
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/2525-count-number-of-distinct-integers-after-reverse-operations.ts
@@ -0,0 +1,19 @@
+function countDistinctIntegers(nums: number[]): number {
+    let ans = [...nums];
+    for(let i = 0; i < nums.length; i++){
+        let temp = reverse(nums[i]);
+        ans.push(temp);
+    }
+    
+   let a = new Set([...ans])
+   return a.size
+};
+
+const reverse = (x: number) => {
+    let rev = 0;
+    while(x > 0){
+        rev = rev * 10 + x % 10;
+        x = Math.floor(x/10)
+    }
+    return Number(rev)
+}
