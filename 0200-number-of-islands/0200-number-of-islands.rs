db57a490b40a3688872c08db478c89b242f5edae
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-09-19 16:39:37 +0100

    Time: 8 ms (84.13%), Space: 9.1 MB (48.81%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.rs
+++ 0200-number-of-islands/0200-number-of-islands.rs
@@ -1,37 +1,63 @@
+use std::collections::VecDeque;
+
 const DIRS: [(i32, i32); 4] = [(0, 1), (1, 0), (-1, 0), (0, -1)];
 
 impl Solution {
     pub fn num_islands(grid: Vec<Vec<char>>) -> i32 {
         let mut grid = grid;
-        let (row, col) = (grid.len(), grid[0].len());
-        let mut count = 0;
+        let (row, col, mut num_of_islands) = (grid.len(), grid[0].len(), 0);
+
+        let mut q: VecDeque<(usize, usize)> = VecDeque::with_capacity(row * col);
 
         for i in 0..row {
             for j in 0..col {
                 if grid[i][j] == '1' {
-                    count += 1;
-                    //perform dfs
-                    Self::dfs(i as i32, j as i32, row as i32, col as i32, &mut grid);
+                    num_of_islands += 1;
+                    //Approach 1: perform dfs
+                    // Self::dfs(i as i32, j as i32, row as i32, col as i32, &mut grid);
+
+                    //Approach 2: perform bfs
+
+                    //push grid position of island into thr queue
+                    q.push_back((i, j));
+                    //search for islands in the queue, and try to find connecting islands and mark them
+                    while !q.is_empty(){
+                        for _ in 0..q.len() {
+                            let (k, l) = q.pop_front().unwrap();
+                                //boundary and validity checks
+                                if k < 0 || l < 0 || k >= row || l >= col || grid[k as usize][l as usize] == '0' {
+                                    continue;
+                                }
+                                grid[k as usize][l as usize] = '0';
+                                
+                                //push all adjacent edges into the queue
+                                for dir in DIRS {
+                                    q.push_back((k + dir.0 as usize, l + dir.1 as usize));
+                                }
+                        }
+                    }
+
+
                 }
             }
         }
 
-        return count;
+        return num_of_islands;
     }
 
-    pub fn dfs(i: i32, j: i32, row: i32, col: i32, grid: &mut Vec<Vec<char>>) {
-        //boundary and validity checks
-        if i < 0 || j < 0 || i >= row || j >= col || grid[i as usize][j as usize] == '0' {
-            return;
-        }
+    // pub fn dfs(i: i32, j: i32, row: i32, col: i32, grid: &mut Vec<Vec<char>>) {
+    //     //boundary and validity checks
+    //     if i < 0 || j < 0 || i >= row || j >= col || grid[i as usize][j as usize] == '0' {
+    //         return;
+    //     }
 
-        grid[i as usize][j as usize] = '0';
+    //     grid[i as usize][j as usize] = '0';
 
-        for dir in DIRS {
-            let i_ = i + dir.0;
-            let j_ = j + dir.1;
+    //     for dir in DIRS {
+    //         let i_ = i + dir.0;
+    //         let j_ = j + dir.1;
 
-            Self::dfs(i_, j_, row, col, grid);
-        }
-    }
+    //         Self::dfs(i_, j_, row, col, grid);
+    //     }
+    // }
 }
