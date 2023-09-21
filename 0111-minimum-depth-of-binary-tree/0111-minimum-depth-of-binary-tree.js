727495466d3c54d6f0c566355959bca7a4479dc4
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-09-22 00:05:55 +0100

    Time: 193 ms (30.44%), Space: 99.1 MB (90.49%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
@@ -12,9 +12,23 @@
  */
 var minDepth = function(root) {
         if(!root) return 0;
-    if(root.right !== null && root.left !== null){
-        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
-    } else {
-        return 1 + Math.max(minDepth(root.left), minDepth(root.right));
+    // if(root.right !== null && root.left !== null){
+    //     return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    // } else {
+    //     return 1 + Math.max(minDepth(root.left), minDepth(root.right));
+    // }
+
+    let queue = [root];
+    let height = 0;
+
+    while(queue.length > 0){
+        let len = queue.length;
+        height++;
+        for(let i = 0; i < len; i++){
+            let node = queue.shift();
+            if(!node.left && !node.right) return height;
+            if(node.left) queue.push(node.left)
+            if(node.right) queue.push(node.right)
+        }
     }
 };
