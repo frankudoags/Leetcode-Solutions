94d00b3bd833b36280e90b95c28a87f298ae00d8
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-10-01 20:30:39 +0100

    Time: 95 ms (57.85%), Space: 51 MB (74.33%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.ts
@@ -0,0 +1,36 @@
+class MinStack {
+    stack: number[][]
+    constructor() {
+        this.stack = [];
+    }
+
+    push(val: number): void {
+        if(this.stack.length == 0) {
+            this.stack.push([val, val])
+            return
+        }
+
+        this.stack.push([val, Math.min(val, this.stack[this.stack.length - 1][1])])
+    }
+
+    pop(): void {
+        this.stack.pop();
+    }
+
+    top(): number {
+        return this.stack[this.stack.length - 1][0]
+    }
+
+    getMin(): number {
+        return this.stack[this.stack.length - 1][1]
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
