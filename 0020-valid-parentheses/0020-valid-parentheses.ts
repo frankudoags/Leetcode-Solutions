74b01a590036ec84b1d02a345bf70fec23975733
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-10-02 09:22:09 +0100

    Time: 55 ms (78.55%), Space: 44.8 MB (35.09%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.ts
+++ 0020-valid-parentheses/0020-valid-parentheses.ts
@@ -3,20 +3,14 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-    s = s.trim().split('');
     if(s.length % 2 !== 0) return false
+    s = s.trim().split('');
     const stack: Array<string> = [];
     const couple: Record<string, string> = { ")": "(", "]": "[", "}": "{" };
 
     for (let char of s) {
-        if (!couple[char]) {
-            stack.push(char);
-            continue;
-        }
-        if(couple[char] && stack.length == 0) return false
-        if (stack.length && couple[char] != stack.pop()) {
-            return false
-        }
+        if (!couple[char]) stack.push(char);
+        else if (couple[char] != stack.pop()) return false
     }
 
     return stack.length == 0
