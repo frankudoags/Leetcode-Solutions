44fb1646d043d6f92cd8fb756048527b29279529
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2023-10-16 15:07:57 +0100

    Time: 57 ms (49.66%), Space: 44.3 MB (39.46%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.ts
@@ -0,0 +1,63 @@
+function calcEquation(equations: string[][], values: number[], queries: string[][]): number[] {
+    let graph = buildGraph(equations, values);
+    let res: number[] = []
+    let visited = new Set<string>();
+
+    for (let i = 0; i < queries.length; i++) {
+        let [src, des] = [queries[i][0], queries[i][1]];
+        if (!graph[src] || !graph[des]) {
+            res.push(-1.0);
+        }
+        else {
+            res.push(dfs(src, des, visited, graph))
+            visited = new Set()
+        }
+    }
+
+    return res
+};
+
+function dfs(
+    src: string,
+    des: string,
+    visited: Set<string>,
+    graph: Record<string, Edge[]>
+): number {
+    if (src == des) return 1.0
+    if (visited.has(src)) return -1.0
+    visited.add(src);
+    for (let edge of graph[src]) {
+        let res = dfs(edge.des, des, visited, graph)
+        if (res != -1.0) {
+        return edge.val * res
+        }
+    }
+    return -1.0
+}
+/**
+{
+  a: [ { des: 'b', val: 2 } ],
+  b: [ { des: 'a', val: 0.5 }, { des: 'c', val: 3 } ],
+  c: [ { des: 'b', val: 0.3333333333333333 } ]
+}
+ */
+
+function buildGraph(equations: string[][], values: number[]): Record<string, Edge[]> {
+    let graph: Record<string, Edge[]> = {}
+
+    for (let i = 0; i < equations.length; i++) {
+        let [src, des, val] = [equations[i][0], equations[i][1], values[i]];
+
+        if (!graph[src]) graph[src] = []
+        graph[src].push({ des, val })
+
+        if (!graph[des]) graph[des] = []
+        graph[des].push({ des: src, val: 1.0 / val })
+    }
+    return graph
+}
+
+type Edge = {
+    des: string;
+    val: number;
+}
